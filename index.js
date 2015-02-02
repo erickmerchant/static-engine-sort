a0c2bc5a090b33598e10f5d19e0536fd997d955b
Erick Merchant <erickmerchant@gmail.com>
2015-02-01 21:18:38 -0800

    Add more sorts

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 var now = require('moment')();
 
+function date_sort(pages) {
+
+    pages.sort(function(a, b) {
+
+        a = a.date || now;
+
+        b = b.date || now;
+
+        return b.diff(a);
+    });
+
+    return pages;
+}
+
+function date(pages, done) {
+
+    done(null, date_sort(pages));
+}
+
+date.desc = date;
+
+date.asc = function (pages, done) {
+
+    done(null, date_sort(pages).reverse());
+}
+
+function title_sort(pages) {
+
+    pages.sort(function(a, b) {
+
+        a = a.title || '';
+
+        b = b.title || '';
+
+        return a.localeCompare(b);
+    });
+
+    return pages;
+}
+
+function title(pages, done) {
+
+    done(null, title_sort(pages));
+}
+
+title.asc = title;
+
+title.desc = function (pages, done) {
+
+    done(null, title_sort(pages).reverse());
+}
+
 module.exports = {
 
-    date: function (pages, done) {
+    date: date,
 
-        pages.sort(function(a, b) {
-
-            a = a.date || now;
-
-            b = b.date || now;
-
-            return b.diff(a);
-        });
-
-        done(null, pages);
-    }
+    title: title
 };
