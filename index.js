a5714aca28710c1a36161ae4f774798bcec85ac4
Erick Merchant <erickmerchant@gmail.com>
2015-04-08 18:53:50 -0700

    Change everything

--- index.js
+++ index.js
@@ -1,58 +1,9 @@
-var now = require('moment')();
+module.exports = function(sorter) {
 
-function sortDate(pages, reverse) {
+    return function (pages, done) {
 
-    pages.sort(function(a, b) {
+        pages.sort(sorter);
 
-        a = a.date || now;
-
-        b = b.date || now;
-
-        return reverse ? b.diff(a) : a.diff(b);
-    });
-
-    return pages;
-}
-
-function sortTitle(pages, reverse) {
-
-    pages.sort(function(a, b) {
-
-        a = a.title || '';
-
-        b = b.title || '';
-
-        return reverse ? b.localeCompare(a) : a.localeCompare(b);
-    });
-
-    return pages;
-}
-
-module.exports = {
-
-    date: {
-
-        asc: function (pages, done) {
-
-            done(null, sortDate(pages));
-        },
-
-        desc: function (pages, done) {
-
-            done(null, sortDate(pages, true));
-        }
-    },
-
-    title: {
-
-        asc: function (pages, done) {
-
-            done(null, sortTitle(pages));
-        },
-
-        desc: function (pages, done) {
-
-            done(null, sortTitle(pages, true));
-        }
-    }
+        done(null, pages);
+    };
 };
--- test/test.js
+++ test/test.js
@@ -1,58 +1,18 @@
 var assert = require('assert');
-var sort = require('../index.js');
-var moment = require('moment');
+var sort = require('../index.js')(function(a, b){
+
+    return a.title.localeCompare(b.title);
+});
 
 describe('plugin', function(){
 
-    describe('title.asc', function(){
+    it('it should sort using the provided function', function(done){
 
-        it('it should sort titles in alphabetical order', function(done){
+        sort([{ title: 'réservé' }, { title: 'premier' }, { title: 'cliché' }, { title: 'communiqué' }, { title: 'café' }, { title: 'adieu' }], function(err, pages){
 
-            sort.title.asc([{ title: 'réservé' }, { title: 'premier' }, { title: 'cliché' }, { title: 'communiqué' }, { title: 'café' }, { title: 'adieu' }], function(err, pages){
+            assert.deepEqual(pages, [ { title: 'adieu' }, { title: 'café' }, { title: 'cliché' }, { title: 'communiqué' }, { title: 'premier' }, { title: 'réservé' } ]);
 
-                assert.deepEqual(pages, [ { title: 'adieu' }, { title: 'café' }, { title: 'cliché' }, { title: 'communiqué' }, { title: 'premier' }, { title: 'réservé' } ]);
-
-                done();
-            });
-        });
-    });
-
-    describe('title.desc', function(){
-
-        it('it should sort titles in reverse alphabetical order', function(done){
-
-            sort.title.desc([{ title: 'réservé' }, { title: 'premier' }, { title: 'cliché' }, { title: 'communiqué' }, { title: 'café' }, { title: 'adieu' }], function(err, pages){
-
-                assert.deepEqual(pages, [ { title: 'réservé' }, { title: 'premier' }, { title: 'communiqué' }, { title: 'cliché' }, { title: 'café' }, { title: 'adieu' } ]);
-
-                done();
-            });
-        });
-    });
-
-    describe('date.asc', function(){
-
-        it('it should sort dates oldest to newest', function(done){
-
-            sort.date.asc([{date: moment('20120101', 'YYYYMMDD')}, {date: moment('20110101', 'YYYYMMDD')}, {date: moment('20130101', 'YYYYMMDD')}], function(err, pages){
-
-                assert.deepEqual(pages, [{date: moment('20110101', 'YYYYMMDD')}, {date: moment('20120101', 'YYYYMMDD')}, {date: moment('20130101', 'YYYYMMDD')}]);
-
-                done();
-            });
-        });
-    });
-
-    describe('date.desc', function(){
-
-        it('it should sort dates newest to oldest', function(done){
-
-            sort.date.desc([{date: moment('20120101', 'YYYYMMDD')}, {date: moment('20110101', 'YYYYMMDD')}, {date: moment('20130101', 'YYYYMMDD')}], function(err, pages){
-
-                assert.deepEqual(pages, [{date: moment('20130101', 'YYYYMMDD')}, {date: moment('20120101', 'YYYYMMDD')}, {date: moment('20110101', 'YYYYMMDD')}]);
-
-                done();
-            });
+            done();
         });
     });
 });
